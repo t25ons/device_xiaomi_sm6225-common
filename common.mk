037d56b785e067924f44fe39209dbbe4dfc0bfd8
SamarV-121 <samarvispute121@pm.me>
2023-02-07 12:11:15 +0100

    sm6225-common: Disable continuous transaction tracing on all build types
    Change-Id: I95657a00e38f1229675682e784b20cca12c119b0
    Signed-off-by: Parixshit <87859520+Parixshit@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -237,6 +237,7 @@ PRODUCT_VENDOR_PROPERTIES += \
     debug.sf.disable_backpressure=1 \
     debug.sf.enable_hwc_vds=0 \
     debug.sf.enable_egl_image_tracker=false \
+    debug.sf.enable_transaction_tracing=false \
     debug.sf.predict_hwc_composition_strategy=0 \
     debug.sf.layer_caching_active_layer_timeout_ms=1000 \
     debug.cpurend.vsync=false \
